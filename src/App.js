72d551535770ba33a6aa77f808a80d43ba2c4b69
Dinalk <124670219+Dinalk@users.noreply.github.com>
2023-02-07 19:58:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Counter from "./Counter/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Counter</h1>
+        <Counter />
+      </header>
+    </div>
+  );
+}
+export default App;
---
+++ src/Counter/Counter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Counter extends React.Component {
+  render() {
+    return (
+      <div className="counterApp">
+        <button>+1</button>
+        <label>0</label>
+      </div>
+    );
+  }
+}
+export default Counter;
